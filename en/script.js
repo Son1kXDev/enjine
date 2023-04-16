92337ea4a3ea81846370760351cc27e15c21fcad
Son1kX <son1kx@vk.com>
2023-04-17 02:40:42 +0300

    Проба переноса игр в отдельнный файл

--- en/script.js
+++ en/script.js
@@ -1,23 +1,4 @@
-const games = [
-  {
-    title: 'Offroaded',
-    engine: '<i class="fa-brands fa-unity"></i>',
-    releaseDate: '<i class="fa-solid fa-infinity"></i>',
-    status: 'in development',
-    version: '',
-    image: '../images/preview/offroaded-preview.png',
-    link: ''
-  },  
-  {
-    title: '5opka Game',
-    engine: '<i class="fa-brands fa-unity"></i>',
-    releaseDate: '<i class="fa-solid fa-infinity"></i>',
-    status: 'in development',
-    version:'',
-    image: '../images/preview/5opka-preview.jpg',
-    link: ''
-  }
-];
+import games from './js/games.js';
 
 function generateGamesList() {
   const gamesList = document.getElementById('games-list');
---
+++ js/games.js
@@ -0,0 +1,22 @@
+const games = [
+    {
+      title: 'Offroaded',
+      engine: '<i class="fa-brands fa-unity"></i>',
+      releaseDate: '<i class="fa-solid fa-infinity"></i>',
+      status: 'in development',
+      version: '0.1.1',
+      image: '../images/preview/offroaded-preview.png',
+      link: 'youtube.com'
+    },  
+    {
+      title: '5opka Game',
+      engine: '<i class="fa-brands fa-unity"></i>',
+      releaseDate: '<i class="fa-solid fa-infinity"></i>',
+      status: 'in development',
+      version:'',
+      image: '../images/preview/5opka-preview.jpg',
+      link: ''
+    }
+  ];
+
+  export default games;
