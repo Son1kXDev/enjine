6dabe7423a0a5b81d59d656bdca5325ba893b5de
Son1kX <son1kx@vk.com>
2023-04-24 03:35:42 +0300

    Сделал красивыми ссылки

--- js/autoLanguage.js
+++ js/autoLanguage.js
@@ -4,8 +4,8 @@ var userLang = navigator.language || navigator.userLanguage;
 // Определяем, что делать в зависимости от языка пользователя
 if (userLang.indexOf('ru') === 0) {
   // Пользователь использует русский язык
-  window.location.href = '/ru/home.html';
+  window.location.href = '/ru/home';
 } else {
   // Пользователь использует любой другой язык
-  window.location.href = '/en/home.html';
+  window.location.href = '/en/home';
 }
