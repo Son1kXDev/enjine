54375979a10140e4ec753a3f5762726e77ad72cb
Son1kX <son1kx@vk.com>
2023-04-26 17:19:02 +0300

    изменил статус offroaded на бета

--- js/games.js
+++ js/games.js
@@ -19,8 +19,8 @@ const games = [
       engine: '<i class="fa-brands fa-unity"></i>',
       releaseDate: '<i class="fa-solid fa-infinity"></i>',
       status: {
-        ru: 'в разработке',
-        en: 'in development'
+        ru: 'БЕТА',
+        en: 'BETA'
       },
       version: '0.1.0 beta',
       image: '../images/preview/offroaded-preview.png',
