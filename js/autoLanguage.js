40b35ade285955531387eb0fe290b277c0ac8c5d
Son1kX <son1kx@vk.com>
2023-05-01 07:53:18 +0300

    Обновил стили. Разделил файл style.css на несколько отдельных

--- js/autoLanguage.js
+++ js/autoLanguage.js
@@ -1,11 +1,7 @@
-// Получаем язык браузера пользователя
 var userLang = navigator.language || navigator.userLanguage;
 
-// Определяем, что делать в зависимости от языка пользователя
 if (userLang.indexOf('ru') === 0) {
-  // Пользователь использует русский язык
   window.location.href = '/ru/home';
 } else {
-  // Пользователь использует любой другой язык
   window.location.href = '/en/home';
 }
