239a195868bc448d2d4b4efb8ed756c2391f1e41
Son1kX <son1kx@vk.com>
2023-05-03 03:37:14 +0300

    Сделал для описания игры отдельную страницу

--- js/games.js
+++ js/games.js
@@ -1,21 +1,6 @@
 const games = [
     {
       title: 'Offroaded',
-      description: {
-        ru: '<h3>Добро пожаловать в захватывающий мир нашей новой игры!</h3><p>Погрузитесь в захватывающие виртуальные поездки по различным картам нашей игры! Выполняйте миссии, улучшайте и настраивайте свой автомобиль, покупайте новые машины и делайте их уникальными, настраивая спойлеры, сплиттеры и подвеску, а также меняя цвет и добавляя наклейки. Не забудьте следить за регулярным обслуживанием своего автомобиля, заправлять его вовремя и чистить от грязи после каждой захватывающей поездки. Разблокируйте множество достижений и продвиньтесь в игре, исследуя разнообразные карты с различными условиями погоды.</p> <p>Готовы к новым приключениям? Отправляйтесь в наши виртуальные поездки уже сегодня!</p>',
-        en: '<h3>Welcome to the exciting world of our new game!</h3><p>Immerse yourself in thrilling virtual trips to various maps in our game! Complete missions, upgrade and customize your car, buy new cars and make them unique by adjusting spoilers, splitters, suspension, as well as changing color and adding stickers. Don\'t forget to regularly service your car, refuel it on time, and clean it from dirt after each exciting trip. Unlock numerous achievements and progress in the game by exploring diverse maps with various weather conditions.</p> <p>Ready for new adventures? Embark on our virtual trips today!</p>'
-      },
-      descriptionLow: {
-        ru: '<h3>Добро пожаловать в захватывающий мир нашей новой игры!</h3> Мы предлагаем вам виртуальные поездки по картам, выполнение миссий, настройку и улучшение автомобиля, а также исследование различных карт. Полная настройка автомобиля, включая спойлеры, подвеску и цвет, делает вашу машину уникальной. Можно также добавить собственные наклейки. Важно следить за обслуживанием, заправкой и чисткой машины. Игра также предлагает множество миссий, достижений и разнообразных карт с различными условиями. Готовы отправиться в захватывающие приключения? Откройте для себя мир виртуальных поездок в нашей новой игре!',
-        en: '<h3>Welcome to the exciting world of our new game!</h3> We offer you virtual trips on various maps, mission completion, car customization and upgrading, as well as exploring different maps. Full customization of your car, including spoilers, suspension, and color, makes your car truly unique. You can also add your own decals as stickers on your car. It\'s important to keep track of regular car maintenance, refueling, and cleaning after exciting trips. The game also offers numerous missions, achievements, and diverse maps with different conditions. Are you ready for thrilling adventures? Discover the world of virtual trips in our new game!'
-      },
-      screenshots: ['../images/games/offroaded/screenshot_1.jpg',
-      '../images/games/offroaded/screenshot_6.jpg',
-      '../images/games/offroaded/screenshot_5.jpg',
-      '../images/games/offroaded/screenshot_7.jpg',
-      '../images/games/offroaded/screenshot_8.jpg',
-      '../images/games/offroaded/screenshot_9.jpg'],
-      usePopup: true,
       engine: '<i class="fa-brands fa-unity"></i>',
       releaseDate: '<i class="fa-solid fa-infinity"></i>',
       status: {
@@ -24,16 +9,13 @@ const games = [
       },
       version: '0.1.0 beta',
       image: '../images/preview/offroaded-preview.png',
-      link: '../games/offroaded/offroaded_v0.1.0beta.apk'
+      link: {
+        ru: '../ru/offroaded',
+        en: '../en/offroaded'
+        }
     },  
     {
       title: '5opka Game',
-      description: {
-        ru:'',
-        en:''
-      },
-      screenshots: [],
-      usePopup: false,
       engine: '<i class="fa-brands fa-unity"></i>',
       releaseDate: '<i class="fa-solid fa-infinity"></i>',
       status: {
@@ -42,7 +24,10 @@ const games = [
       },
       version:'',
       image: '../images/preview/5opka-preview.jpg',
-      link: ''
+      link: {
+        ru: '',
+        en: ''
+        }
     }
   ];
 
--- ru/script.js
+++ ru/script.js
@@ -1,76 +1,5 @@
 import games from '../js/games.js';
 
-function openGamePopup(game) {
-  const popupContainer = document.createElement('div');
-  popupContainer.classList.add('popup-container');
-
-  const popupContent = document.createElement('div');
-  popupContent.classList.add('popup-content');
-
-  const closeBtn = document.createElement('span');
-  closeBtn.innerHTML = '&times;';
-  closeBtn.classList.add('close-btn');
-  closeBtn.addEventListener('click', function() {
-    var currentUrl = window.location.href;
-    var gameTitle = '#' + game.title;
-    var newUrl = currentUrl.replace(gameTitle, '');
-    window.history.replaceState({}, document.title, newUrl);
-    document.body.classList.remove('popup-open');
-    popupContent.classList.add('popup-content-close');
-    setTimeout(function() {
-      popupContainer.remove();
-    }, 500);
-  });
-
-  const titleEl = document.createElement('h1');
-  titleEl.innerHTML = game.engine + ' ' + game.title;
-
-  const descriptionEl = document.createElement('p');
-  descriptionEl.classList.add('description');
-  descriptionEl.innerHTML = game.description.ru;
-
-  const descriptionLowerEl = document.createElement('div');
-  descriptionLowerEl.classList.add('descriptionLow');
-  descriptionLowerEl.innerHTML = game.descriptionLow.ru;
-
-  const screenshotsEl = document.createElement('div');
-  screenshotsEl.classList.add('screenshots');
-
-  game.screenshots.forEach(screenshot => {
-    const screenshotEl = document.createElement('img');
-    screenshotEl.src = screenshot;
-    screenshotEl.alt = 'Screenshot';
-    screenshotsEl.appendChild(screenshotEl);
-  });
-
-  const versionEl = document.createElement('p');
-  versionEl.classList.add('version');
-  versionEl.innerText = `Версия: ${game.version}`;
-
-  const downloadBtn = document.createElement('button');
-  downloadBtn.innerHTML = '<i class="fa-solid fa-download"></i> Скачать';
-  downloadBtn.classList.add('download-btn');
-  if (game.link === '') {
-    downloadBtn.innerHTML = '<i class="fa-solid fa-clock"></i> Скоро';
-    downloadBtn.disabled = true;
-  } else {
-    downloadBtn.addEventListener('click', () => {
-      window.location.href = game.link;
-    });
-  }
-
-  popupContent.appendChild(closeBtn);
-  popupContent.appendChild(titleEl);
-  popupContent.appendChild(descriptionEl);
-  popupContent.appendChild(descriptionLowerEl);
-  popupContent.appendChild(screenshotsEl);
-  popupContent.appendChild(versionEl);
-  popupContent.appendChild(downloadBtn);
-  popupContainer.appendChild(popupContent);
-  document.body.appendChild(popupContainer);
-  document.body.classList.add('popup-open');
-}
-
 function generateGamesList() {
   const gamesList = document.getElementById('games-list');
   gamesList.innerHTML = '';
@@ -78,16 +7,14 @@ function generateGamesList() {
   games.forEach(game => {
     const li = document.createElement('li');
 
-    if(game.usePopup){
-    const popupLink = document.createElement('a');
-    popupLink.href = '#' + game.title;
-    popupLink.addEventListener('click', () => {
-      openGamePopup(game);
-    });
+    if(game.link.ru != ''){
+    const gameLink = document.createElement('a');
+    gameLink.href = game.link.ru;
     li.addEventListener('click', () => {
-      popupLink.click();
+      gameLink.click();
     });
     } 
+
     const imgEl = document.createElement('img');
     imgEl.src = game.image || '';
     imgEl.alt = game.title;
