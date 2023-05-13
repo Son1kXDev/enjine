f1b2cf07ffb84418bb00ec3a1d32602339a649d8
Son1kX <son1kx@vk.com>
2023-05-13 03:38:42 +0300

    Второй фикс новых ссылок

---
+++ js/autoLanguageIndex.js
@@ -0,0 +1,3 @@
+var userLang = navigator.language || navigator.userLanguage;
+window.location.replace((userLang.indexOf('ru') === 0 ? '/ru/' : '/en/') + "home");
+
--- js/discord.js
+++ js/discord.js
@@ -1,21 +1,21 @@
-var widget = document.querySelector('.discord-widget');
-var openButton = document.getElementById("discord");
-var closeButton = document.getElementById('discord-close-btn');
+window.onload = function(){
+  var widget = document.querySelector('.discord-widget');
+  var openButton = document.getElementById("discord");
+  var closeButton = document.getElementById('discord-close-btn');
 
-widget.querySelector('iframe').style.display = 'none';
-
-openButton.addEventListener('click', function() {
-  openButton.style.display = 'none';
-  closeButton.style.display = 'block';
-  widget.querySelector('iframe').style.display = 'block';
-});
-
-closeButton.addEventListener('click', function() {
-  openButton.style.display = 'block';
-  closeButton.style.display = 'none';
   widget.querySelector('iframe').style.display = 'none';
-});
 
-closeButton.style.display = 'none';
+  openButton.addEventListener('click', function() {
+    openButton.style.display = 'none';
+    closeButton.style.display = 'block';
+    widget.querySelector('iframe').style.display = 'block';
+  });
 
+  closeButton.addEventListener('click', function() {
+    openButton.style.display = 'block';
+    closeButton.style.display = 'none';
+    widget.querySelector('iframe').style.display = 'none';
+  });
 
+  closeButton.style.display = 'none';
+}
